69a3e146b38465910449d34814dd81ade90a9e8e
sumair-khan21 <johnalex21210@gmail.com>
2025-05-29 16:44:45 +0500

    updated 5-29-2025

---
+++ javascript-05-23-2025/app.js
@@ -0,0 +1,102 @@
+// let age =  ("Enter your age: ");
+// let current = age - 18
+// console.log(current);
+
+// console.log(parseInt('18.090'));
+// console.log(parseFloat('18.0900000'));
+
+
+// let num = '18.5900000';
+// console.log(typeof(Math.floor(num)));
+// console.log(typeof(Math.ceil(num)));
+// console.log(typeof(Math.round(num)));
+
+
+
+// toString() number ko string bana deta hai
+// let num = 18.5900000;
+// console.log(typeof(num.toString()));
+
+
+// toFixed() number ko fixed bana deta hai point ke baad koi bhi number de sakhta hy
+// let num2 = 18875.9898;
+// let num2 = 18875.9598;
+// let num2 = 18875.9198;
+// console.log(num2.toFixed(2));
+
+
+// let num = 1.556
+// var str = num.toString();
+// if(str.charAt(str.length - 1)=== "5"){
+//     str = str.slice(0, str.length -1) + "6";
+// }
+// num = Number(str)
+// pre = num.toFixed(2)
+// console.log(pre);
+
+
+
+// date and time
+
+// let date = new Date();
+// console.log(date);
+
+
+// let dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// let today = new Date()
+// console.log("Today is",dayName[today.getDay()]);
+// console.log("Today is",today.getMonth());
+// console.log("Today is",today.getDay());
+// console.log("Today is",today.getFullYear());
+// console.log("Today is",today.getHours());
+// console.log("Today is",today.getMinutes());
+// console.log("Today is",today.getSeconds());
+// console.log("Today is",today.getMilliseconds());
+
+
+
+// let today = new Date("2025-05-23");
+// console.log(today);
+// // console.log("Today is",dayName[today.getDay()]);
+// console.log("Today is",today.getMonth());
+// console.log("Today is",today.getDay());
+// console.log("Today is",today.getFullYear());
+// console.log("Today is",today.getHours());
+// console.log("Today is",today.getMinutes());
+// console.log("Today is",today.getSeconds());
+// console.log("Today is",today.getMilliseconds());
+
+
+
+// let today = new Date();
+// let dob = new Date('Dec 21 2002')
+// console.log(dob);
+// console.log("Today is",today.getMonth());
+// console.log("Today is",today.getDay());
+// console.log("Today is",today.getFullYear());
+// console.log("Today is",today.getHours());
+// console.log("Today is",today.getMinutes());
+// console.log("Today is",today.getSeconds());
+// console.log("Today is",today.getMilliseconds());
+
+
+// let today = new Date();
+// let dob = new Date('Jul 16 2002')
+
+// let todayYear = today.getFullYear()
+// let dobYear = dob.getFullYear()
+
+// let age = todayYear - dobYear
+// console.log("Age", + age);
+
+// var today = new Date();
+// var doomsday = new Date("June 30, 2035");
+// var msToday = today.getTime();
+// var msDoomsday = doomsday.getTime();
+// var msDiff = msDoomsday - msToday;
+// var dDiff = msDiff / (1000 * 60 * 60 * 24);
+// console.log(dDiff);
+
+
+
+
---
+++ javascript-05-26-2025/app.js
@@ -0,0 +1,40 @@
+// todatestring
+
+// let today = new Date();
+// // ye todate string mai convert karega
+// console.log(today.toDateString());
+// console.log(today.toLocaleDateString());
+
+// console.log(today.toLocaleString());
+// console.log(today.toLocaleTimeString());
+
+
+// leap year 2025 to 2050
+
+// let today = new Date("2050-01-01");
+
+
+
+
+// let count = 0;
+
+// for(let year = 2025; year <= 3035  ; year++){
+//     if((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0){
+//         console.log(year, "leap year");
+//         count++
+//     }
+// }
+
+// console.log(count);
+
+
+
+
+
+
+
+
+
+
+
+
