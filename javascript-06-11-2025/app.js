b90e585c1c8d918663c68fcf3842f764a71cc6cf
sumair-khan21 <johnalex21210@gmail.com>
2025-06-12 21:12:27 +0500

    6/12/2025

---
+++ javascript-06-11-2025/app.js
@@ -0,0 +1,81 @@
+// Setting field values
+ 
+ 
+ function fillCity() {
+ var cityName;
+ var zipEntered = document.getElementById("zip").value;
+ switch (zipEntered) {
+ case "60608" :
+ cityName = "Chicago";
+ break;
+ case "68114" :
+ cityName = "Omaha";
+break;
+case "53212" :
+cityName = "Milwaukee";
+}
+     document.getElementById("city").value = cityName;
+     }
+
+
+    //  Reading and setting paragraph text
+
+
+
+ function expandLoris() {
+ var expandedParagraph = `Slow lorises are a group of several species of trepsirrhine primates which
+    make up the genus Nycticebus. They have a round head, narrow snout, large eyes, and a variety of
+    distinctive coloration patterns that are species-dependent. The hands and feet of slow lorises have several
+    adaptations that give them a pincer-like grip and enable them to grasp branches for long periods of time.
+    Slow lorises have a toxic bite, a rare trait among mammals.`;
+ document.getElementById("slowLoris").innerHTML = expandedParagraph;
+ }
+
+
+
+ function placeAList() {
+ var listToPlace = "<ol><li>Slow loris</li><li>Fast loris</li><li>Just-right loris</li></ol>";
+ document.getElementById("lorisList").innerHTML = listToPlace;
+ }
+    
+
+ function peekAtContent() {
+ var whatsThere = document.getElementById("content").innerHTML;
+ }
+
+
+//  Manipulating images and text
+
+ function makeInvisible() {
+ document.getElementById("ugly").className = "hidden";
+ }
+
+
+
+ function makeBig() {
+     document.getElementById("p1").className += " big";
+}    
+    
+    
+
+// Swapping images
+
+
+
+
+
+
+// Jung Artical
+
+
+
+
+
+
+
+
+
+
+
+
+
