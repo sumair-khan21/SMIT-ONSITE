66e92d26a7156d3769721223b6714d20930a6b8c
sumair-khan21 <johnalex21210@gmail.com>
2025-09-12 14:04:05 +0500

    updated bootstrap with react

---
+++ react-09-12-2025/bootstrap/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import MyNavbar from './MyNavbar'
+import CardSection from './CardSection'
+import HeroSection from './HeroSection'
+
+const App = () => {
+  return (
+    <>
+     <MyNavbar />
+     <CardSection />
+     <CardSection />
+     <HeroSection />
+    
+    </>
+  )
+}
+
+export default App
---
+++ react-09-12-2025/bootstrap/src/HeroSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <>
+    <section>
+        <div className='container flex justify-content-center align-items-center' style={{height:"80vh"}}>
+            <h1 className='text-center'>Welcome to Hero Section</h1>
+        </div>
+        <div>
+            <p className='text-center'>This is Hero Section</p>
+            
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default HeroSection
