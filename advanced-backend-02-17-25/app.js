fe72e79c69aad1b4bd2ae26b1dc2442011f2a4d6
sumair-khan21 <johnalex21210@gmail.com>
2026-02-17 18:09:05 +0500

    update

---
+++ advanced-backend-02-17-25/app.js
@@ -0,0 +1,7 @@
+// docker!!
+// compass 
+// compass se hum apna network bana sakte hai jisme hum apne container ko connect kar sakte hai. is k banate se container apne private network nhi banate or compass ek bridge network banata hy so frontend or backend container us network se connect ho jate hai or ek dusre ko access kar pate hai.
+
+// docer-compose.yml file me humne do service banayi hai frontend or backend. dono service ko humne compass network se connect kar diya hai. is se dono container ek dusre ko access kar pate hai.
+
+//
